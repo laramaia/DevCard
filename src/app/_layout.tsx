b3b2ef12af101903ac05e92d0216f52843098f54
Lara <laracarolinemaya@gmail.com>
2026-05-15 23:49:50 -0300

    Ajusta _layout.tsx

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,21 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack, Tabs } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { Tabs } from 'expo-router';
+
+import { CardProvider } from './context/CardContext';
 
 export default function RootLayout() {
   return (
-    <Tabs initialRouteName="index" screenOptions={{
-      headerShown: false
-    }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="cadastro" />
-      <Tabs.Screen name="preview" />
-      <Tabs.Screen name="sucesso" />
-    </Tabs>
-  )
+    <CardProvider>
+      <Tabs
+        initialRouteName="index"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Tabs.Screen name="index" />
+        <Tabs.Screen name="cadastro" />
+        <Tabs.Screen name="preview" />
+        <Tabs.Screen name="sucesso" />
+      </Tabs>
+    </CardProvider>
+  );
 }
